52d40c7338f8ab8cde712889b152eadc1f0eeeab
unknown <harsharay.ray@gmail.com>
2020-07-23 21:39:56 +0530

    Updates

---
+++ precache-manifest.a5d1cf303748b770332e2ec57e1d8216.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e864b43cd546cf2d75fee91db7dcbc",
+    "url": "/todo-firebase/index.html"
+  },
+  {
+    "revision": "5765cd414f8052ab844a",
+    "url": "/todo-firebase/static/css/main.6e1d4974.chunk.css"
+  },
+  {
+    "revision": "ba5ce12684cb61a399fd",
+    "url": "/todo-firebase/static/js/2.c65feefe.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/todo-firebase/static/js/2.c65feefe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5765cd414f8052ab844a",
+    "url": "/todo-firebase/static/js/main.84810121.chunk.js"
+  },
+  {
+    "revision": "0dce02fe233f003fdb9e",
+    "url": "/todo-firebase/static/js/runtime-main.6098d0cc.js"
+  }
+]);
